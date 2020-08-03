a76a8e7e1b3bbe994c68b105257850ac4afe83fb
GradilSvyat <43150486+GradilSvyat@users.noreply.github.com>
2020-08-03 14:22:03 +0300

    refactoring

--- CSharpTest/WorkDayCalculator.cs
+++ CSharpTest/WorkDayCalculator.cs
@@ -11,9 +11,9 @@ namespace CSharpTest
             if (dayCount < 1)                   // check input value dayCount
                 throw new ArgumentException("No working days"); 
             DateTime result = startDate.AddDays(dayCount-1);
-            List<DateTime> allWeekEnds = new List<DateTime>();
             if (weekEnds != null)
             {
+                List<DateTime> allWeekEnds = new List<DateTime>();
                 foreach (var item in weekEnds)      // creating a list of all holidays
                 {
                     allWeekEnds.Add(item.StartDate);
@@ -23,11 +23,11 @@ namespace CSharpTest
                         allWeekEnds.Add(item.StartDate);
                     }
                 }
-            }
-            foreach(var day in allWeekEnds)         // changing the end date by the number of days off
-            {
-                if (day >= startDate && day <= result)
-                    result = result.AddDays(1);
+                foreach (var day in allWeekEnds)         // changing the end date by the number of days off
+                {
+                    if (day >= startDate && day <= result)
+                        result = result.AddDays(1);
+                }
             }
             return result;
         }
